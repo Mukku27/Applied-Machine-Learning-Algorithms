d3e61b56db25fcaaf54d13b6e8fdbebc0d003dcd
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2024-09-04 20:23:24 +0530

    elbow method added

---
+++ Unsupervised Learning/Clustering/K means Clustering/k_means.py
@@ -0,0 +1,58 @@
+# demonstration of k-means clustering with iris dataset
+
+
+from sklearn.cluster import KMeans
+import sklearn.datasets
+import matplotlib.pyplot as plt
+
+dataset = sklearn.datasets.load_iris(as_frame=True)
+
+X = (dataset
+     ['data']
+     .loc[:, ['sepal length (cm)', 'sepal width (cm)']]
+)
+y = dataset['target']
+
+# keep list of all centroids
+centroids = []
+
+
+for i in range(10):
+    model = KMeans(n_clusters=3, init='random', n_init=1,
+                    max_iter=i+1, random_state=42)
+    model.fit(X)
+    label = model.predict(X)
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    X.plot.scatter(x='sepal length (cm)', y='sepal width (cm)', c=label, cmap='viridis', ax=ax)
+    
+    centers = model.cluster_centers_
+    ax.scatter(centers[:, 0], centers[:, 1], marker='*', s=250, color='r', alpha=.5)
+    ax.set_title('Iteration: ' + str(i))
+
+    if i > 0:
+        for centroid in centroids:
+            ax.scatter(centroid[:, 0], centroid[:, 1], marker='*', s=250, color='r', alpha=.1)
+    
+    centroids.append(model.cluster_centers_)
+    
+  
+fig, ax = plt.subplots(figsize=(8, 6))
+X.plot.scatter(x='sepal length (cm)', y='sepal width (cm)', c=y, cmap='viridis', ax=ax)
+ax.set_title('Original Data')
+# Running  elbow method to find optimal number of clusters
+
+inertias = []
+max_clusters = 20
+for i in range(max_clusters):
+    km = KMeans(n_clusters=i+1, n_init='auto',
+                max_iter=300, random_state=42)
+    km.fit(X)
+    inertias.append(km.inertia_)
+
+# plot the results
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.plot(range(1, max_clusters+1), inertias, marker='o')
+ax.set_xlabel('Number of clusters')
+ax.set_ylabel('Inertia')
+ax.set_title('Elbow Method')
